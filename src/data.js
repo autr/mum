81ade07d240950c8ba87dc3c61ce45e7803e26c2
Autr <g@sinnott.cc>
2022-02-28 23:09:52 +0000

    init commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export default {
+	
+
+	title: `Iiris Pursiainen BA (Hons) Fine Art.`,
+	subtitle: `A Finnish artist living in Wales.`,
+	statement: `Since gaining my art degree I have experimented with many different methods of making art, such as painting, photography, print-making, collages, fabric-wrapping assemblages, papier coupe. The last couple of years have been a time of mostly making marks and objects by using traditional handicraft methods and materials. These include hand-sewing and machine-sewing on paper, on fabrics, and on painted paper surfaces. My large knitting project, consisting of about 150 strips and panels, continues to grow, until I decide what it is!`,
+	email: `iiris_pursiainen@hotmail.co.uk`
+}
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+	import data from '../data.js'
+
+	const { title, subtitle, statement, email } = data
+
+</script>
+
+
+<svelte:head>
+  <title>Iiris Pursiainen - A Finnish artist living in Wales</title>
+  <meta name="description" content={statement}>
+</svelte:head>
+
+<main class="sassis fill flex column-center-flex-start overflow-auto">
+
+	<div class="flex column maxw70em cmb2 font3 pt2 plr2 ptb8">
+		<header class="f4 cmb0-2">
+			<h1 class="f4">{title}</h1>
+			<h2 class="f4">{subtitle}</h2>
+		</header>
+
+		<!-- <aside class="sink">
+			<img src="img/sheep.jpg" class="w100pc" />
+		</aside> -->
+
+		<article class="font3 f5">
+			<blockquote>{statement}</blockquote>
+		</article>
+
+		<footer class="f3">
+			<a href="mailto:{email}">{email}</a>
+		</footer>
+	</div>
+	
+</main>
